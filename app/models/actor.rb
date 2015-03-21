1e3ba02dc1902251e1508ba108a80e6ee4aaa3d5
Simon Ansquer <simon.ansquer@y-nov.com>
2015-03-21 15:24:44 +0100

    delete some files

--- app/models/actor.rb
+++
@@ -1,9 +0,0 @@
-class Actor
-	include MongoMapper::Document
-
-	#field :id, type:String
-	#field :login, type:String
-	#field :gravatar_id, type:String
-	#field :url, type:String
-	#field :avatar_url, type:String
-end
--- config/initializers/mongo.rb
+++
@@ -1,8 +0,0 @@
-MongoMapper.connection = Mongo::MongoClient.new('ds059907.mongolab.com', 59907, :pool_timeout => 5)
-MongoMapper.database = "chat-#{Rails.env}"
-
-if defined? (PhusionPassenger)
-	PhusionPassenger.on_event(:starting_worker_process) do |forked|
-		MongoMapper.connection.connect if forked
-	end
-end
