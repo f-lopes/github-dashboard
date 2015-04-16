2c6ab012217436c2926c6824cb3a4e85690cb80f
Florian Lopes <florian.lopes@outlook.com>
2015-04-16 14:05:16 +0200

    Return events as json

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   get '/events' => 'events#index'
 
+  get '/events/:id' => 'events#show', as: :id
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
